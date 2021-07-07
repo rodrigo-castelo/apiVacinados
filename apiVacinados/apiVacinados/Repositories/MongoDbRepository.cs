5edacc16b2f306287600fd1daa6cf5c39b655e0a
rodrigo-castelo <81780228+rodrigo-castelo@users.noreply.github.com>
2021-07-07 16:13:21 -0300

    Add files via upload

---
+++ apiVacinados/apiVacinados/Repositories/MongoDbRepository.cs
@@ -0,0 +1,45 @@
+﻿using apiVacinados.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiVacinados.Repositories
+{
+    public class MongoDbRepository
+    {
+        public IMongoDatabase DB { get; }
+        public MongoDbRepository(IConfiguration configuration)
+        {
+            try
+            {
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(configuration["ConnectionString"]));
+                var client = new MongoClient(settings);
+                DB = client.GetDatabase(configuration["NomeBanco"]);
+                MapClasses();
+            }
+            catch (Exception ex)
+            {
+                throw new MongoException("It was not possible to connect to MongoDB", ex);
+            }
+        }
+
+        private void MapClasses()
+        {
+            var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+            if (!BsonClassMap.IsClassMapRegistered(typeof(VacinadoInputModel)))
+            {
+                BsonClassMap.RegisterClassMap<VacinadoInputModel>(i =>
+                {
+                    i.AutoMap();
+                    i.SetIgnoreExtraElements(true);
+                });
+            }
+        }
+    }
+}
